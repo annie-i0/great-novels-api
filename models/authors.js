dc55121ba937d51fff9c04960d5e7752829bb19b
Annie <ikram.annie0@gmail.com>
2021-05-23 22:45:34 -0400

    adding models files

--- models/authors.js
+++ models/authors.js
@@ -2,9 +2,8 @@ const authors = (connection, Sequelize,) => {
   return connection.define('authors',
     {
       id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-      name: { type: Sequelize.STRING, allowNull: false },
-      yearIntroduced: { type: Sequelize.DATEONLY },
-      manufacturerID: { type: Sequelize.INTEGER, references: { model: manufacturers, key: 'id' } },
+      firstName: { type: Sequelize.STRING, allowNull: false },
+      lastName: { type: Sequelize.STRING, allowNull: false },
     }, { paranoid: true })
 }
 
